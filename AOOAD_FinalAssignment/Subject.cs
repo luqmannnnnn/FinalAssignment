8efbb8168d921f6b5f9368af5686083b14274d81
Mei Xin Maxine Tan <s10166940@connect.np.edu.sg>
2018-02-01 11:43:54 +0800

    Added Subject and Observer interface classes and added observer pattern code into policy and client.

---
+++ AOOAD_FinalAssignment/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOOAD_FinalAssignment
+{
+    interface Subject
+    {
+        void registerObserver(Observer o);
+        void removeObserver(Observer o);
+        void notifyObservers();
+
+
+    }
+
+}
